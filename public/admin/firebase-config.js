7ee403fa13b5d42c377323e275bef84acc9f02b9
lippexclash-sys <lippexclash@gmail.com>
2026-02-07 21:59:29 -0300

    Update firebase-config.js

--- public/admin/firebase-config.js
+++ public/admin/firebase-config.js
@@ -1,10 +1,11 @@
-// Firebase v9+ (modular)
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth, setPersistence, browserLocalPersistence } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+// services/firebase.ts
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBo05t4KVpfwF9biqW6DdthsFJAHjrDroE",
+ apiKey: "AIzaSyBo05t4KVpfwF9biqW6DdthsFJAHjrDroE",
   authDomain: "monety-site-2.firebaseapp.com",
   projectId: "monety-site-2",
   storageBucket: "monety-site-2.firebasestorage.app",
@@ -16,5 +17,3 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-setPersistence(auth, browserLocalPersistence);
